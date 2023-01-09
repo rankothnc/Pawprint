2951ef4f90e8c6e2eced530d5f5de8d1a2366897
rankothnc <ncrankoth@gmail.com>
2023-01-09 19:38:07 +0530

    Components

---
+++ src/Pages/EmergencyForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Form from '../Components/Form'
+import '../Styles/EmergencyForm.css'
+
+export default function EmergencyForm() {
+  return (
+    <div>
+    <div class="Wrapper">
+        <Form/>
+    </div>
+    </div>
+  )
+}
