8feefa578b353e34d44f4c71a0aa99af0d9bb387
rankothnc <ncrankoth@gmail.com>
2023-01-19 12:32:58 +0530

    form and title

--- src/Components/Emergency_Contact/Form.js
+++ src/Components/Emergency_Contact/Form.js
@@ -1,16 +1,64 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import '../../Styles/Emergency_Contact/form.css'
+import React from "react";
+import "../../Styles/Emergency_Contact/form.css";
+import { InputBase } from "@mui/material";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-export default function FixedContainer() {
+
+export default function EForm() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Box className='Em-form-container'/>
-      </Container>
-    </React.Fragment>
+    <div>
+      <p className="EmTitle">
+        {" "}
+        <ArrowBackIosIcon className="ArrowBack" />
+        Emergency Contact Information
+      </p>
+      <Box class="boxContainer" sx={{ width: "80%"}}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <p className="InputLabel">Pet Name</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Relationship</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">First Name</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Last Name</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Pri Phone</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Sec Phone</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Email</p>
+            <InputBase className="inp" />
+          </Grid>
+          <Grid item xs={6}>
+            <p className="InputLabel">Street Address </p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Street Address 2 </p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">City</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">State</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Zip Code</p>
+            <InputBase className="inp" />
+
+            <p className="InputLabel">Country</p>
+            <InputBase className="inp" />
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
   );
 }
--- src/Pages/Emergency_Contact/EmergencyForm.js
+++ src/Pages/Emergency_Contact/EmergencyForm.js
@@ -1,77 +1,25 @@
-import React from 'react'
-// import FixedContainer from '../../Components/Emergency_Contact/Form'
-import '../../Styles/Emergency_Contact/EmergencyForm.css'
-// import {TextField} from '@mui/material/TextField'
+import React from "react";
+import "../../Styles/Emergency_Contact/EmergencyForm.css";
 import { TextField, InputBase } from "@mui/material";
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import { styled } from "@mui/material/styles";
+import EForm from "../../Components/Emergency_Contact/Form";
 
 const CssTextField = styled(TextField)({
-  "& .MuiOutlinedInput:after":{
-    borderBlockColor:"green"
-  }
-})
+  "& .MuiOutlinedInput:after": {
+    borderBlockColor: "green",
+  },
+});
 
 export default function EmergencyForm() {
   return (
-    <div className="App">
-     <div className="column left">
-     
-      </div>
+    <div>
+      <div className="column left"></div>
 
       <div className="column right">
-      <div className='container'>
-      <Box class='boxContainer' sx={{ width: '80%' }}>
-      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid item xs={6}>
- 
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp' />
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-        </Grid>
-        <Grid item xs={6}>
-
-        <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-
-          <p className="InputLabel">Pet Name</p>
-          <InputBase className='inp'/>
-        </Grid>
-      </Grid>
-    </Box>
-     
+        <div className="container">
+          <EForm />
+        </div>
       </div>
-     
-      </div>
-      
     </div>
-  )
+  );
 }
